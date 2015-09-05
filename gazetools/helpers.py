0d3c575fc68cee7524f78c4602933d07367314a7
Ryan Hope <rmh3093@gmail.com>
2015-09-05 14:55:44 -0400

    OCLWrapper

--- gazetools/helpers.py
+++ gazetools/helpers.py
@@ -1,4 +1,16 @@
 import pkg_resources
+import pyopencl as cl
 
 def getKernel(filename):
     return pkg_resources.resource_filename("cl",None), pkg_resources.resource_string("cl",filename)
+
+class OCLWrapper(object):
+    def __init__(self):
+        self.ctx = None
+    def build(self, ctx):
+        if self.ctx != ctx:
+            self.ctx = ctx
+            inc, src = getKernel(self.__kernel__)
+            self.prg = cl.Program(ctx, src).build("-I%s" % inc)
+            print dir(self.prg)
+            print self.prg.kernel_names
--- gazetools/imgproc.py
+++ gazetools/imgproc.py
@@ -1,39 +1,27 @@
-from .helpers import getKernel
+from .helpers import OCLWrapper
 
 import pyopencl as cl
 import numpy as np
 import os
 
-class RGB2YCrCb_OCL(object):
+class RGB2YCrCb_OCL(OCLWrapper):
+    __kernel__ = "RGB2YCrCb.cl"
     fmt = cl.ImageFormat(cl.channel_order.RGBA, cl.channel_type.UNSIGNED_INT8)
-    def __init__(self):
-        self.ctx = None
-    def build(self, ctx):
-        if self.ctx != ctx:
-            self.ctx = ctx
-            inc, src = getKernel("RGB2YCrCb.cl")
-            self.prg = cl.Program(ctx, src).build("-I%s" % inc).RGB2YCrCb
     def __call__(self, ctx, src):
         self.build(ctx)
         shape = (src.shape[1], src.shape[0])
         src_buf = cl.image_from_array(self.ctx, src, 4)
         dest_buf = cl.Image(self.ctx, cl.mem_flags.WRITE_ONLY, self.fmt, shape=shape)
         queue = cl.CommandQueue(self.ctx)
-        self.prg(queue, shape, None, src_buf, dest_buf)
+        self.prg.RGB2YCrCb(queue, shape, None, src_buf, dest_buf)
         dest = np.empty_like(src)
         cl.enqueue_copy(queue, dest, dest_buf, origin=(0, 0), region=shape)
         return np.delete(dest, 3, 2)
 RGB2YCrCb = RGB2YCrCb_OCL()
 
-class YCrCb2RGB_OCL(object):
+class YCrCb2RGB_OCL(OCLWrapper):
+    __kernel__ = "YCrCb2RGB.cl"
     fmt = cl.ImageFormat(cl.channel_order.RGBA, cl.channel_type.UNSIGNED_INT8)
-    def __init__(self):
-        self.ctx = None
-    def build(self, ctx):
-        if self.ctx != ctx:
-            self.ctx = ctx
-            inc, src = getKernel("YCrCb2RGB.cl")
-            self.prg = cl.Program(ctx, src).build("-I%s" % inc).YCrCb2RGB
     def __call__(self, ctx, src):
         self.build(ctx)
         shape = (src.shape[1], src.shape[0])
@@ -42,7 +30,7 @@ class YCrCb2RGB_OCL(object):
         src2_buf = cl.image_from_array(self.ctx, src2, 4)
         dest_buf = cl.Image(self.ctx, cl.mem_flags.WRITE_ONLY, self.fmt, shape=shape)
         queue = cl.CommandQueue(self.ctx)
-        self.prg(queue, shape, None, src2_buf, dest_buf)
+        self.prg.YCrCb2RGB(queue, shape, None, src2_buf, dest_buf)
         dest = np.empty_like(src2)
         cl.enqueue_copy(queue, dest, dest_buf, origin=(0, 0), region=shape)
         return np.delete(dest, 3, 2)
