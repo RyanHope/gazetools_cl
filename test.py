8de836328d3088fdc8e78976987799cde782cc85
Ryan Hope <rmh3093@gmail.com>
2015-09-08 21:35:55 -0400

    tweaks to test output

--- test.py
+++ test.py
@@ -12,18 +12,26 @@ from scipy.ndimage import convolve1d as convolve1d_np
 from gazetools import *
 
 ctx = cl.create_some_context()
-fig = plt.figure()
-a=fig.add_subplot(1,2,1)
-src = cv2.cvtColor(cv2.imread(pkg_resources.resource_filename("images","PM5544_with_non-PAL_signals.png"),cv2.IMREAD_UNCHANGED), cv2.COLOR_BGR2RGBA)
-plt.imshow(src)
-a.set_title("orig")
 kernel = [
     [1/16., 1/8., 1/16.],
     [1/8., 1/4., 1/8.],
     [1/16., 1/8., 1/16.],
 ]
+src = cv2.cvtColor(cv2.imread(pkg_resources.resource_filename("images","PM5544_with_non-PAL_signals.png"),cv2.IMREAD_UNCHANGED), cv2.COLOR_BGR2RGBA)
+dest1 = convolve2d(ctx, src, kernel)
+dest2 = convolve2d(ctx, src, kernel)
+print "==============================="
+print dest1
+print "- - - - - - - - - - - - - - - -"
+print dest2
+print "==============================="
+print np.array_equal(dest1,dest2)
+
+fig = plt.figure()
+a=fig.add_subplot(1,2,1)
+plt.imshow(src)
+a.set_title("orig")
 a=fig.add_subplot(1,2,2)
-dest = convolve2d(ctx, src, kernel)
-plt.imshow(dest)
+plt.imshow(dest1)
 a.set_title("blurred")
 plt.show()
