a59c267d5e530de5e9b0aad49429e059076f22d1
Ryan Hope <rmh3093@gmail.com>
2015-09-13 22:31:30 -0400

    collect only

--- python/gazetools/tests/test_imageproc.py
+++ python/gazetools/tests/test_imageproc.py
@@ -14,16 +14,16 @@ from gazetools.imgproc import RGB2YCrCb, YCrCb2RGB
 
 test_image = pkg_resources.resource_filename("gazetools", "resources/images/PM5544_with_non-PAL_signals.png")
 
-# def test_RGB2YCrCb2RGB_uint8():
-#     ctx = cl.create_some_context(0)
-#     src1 = np.asarray(Image.open(test_image))
-#     ycrcb = RGB2YCrCb(ctx, src1)
-#     rgb = YCrCb2RGB(ctx, ycrcb)
-#     assert np.array_equal(src1, rgb)
-#
-# def test_RGB2YCrCb2RGB_float32():
-#     ctx = cl.create_some_context(0)
-#     src2 = mpimg.imread(test_image)
-#     ycrcb = RGB2YCrCb(ctx, src2)
-#     rgb = YCrCb2RGB(ctx, ycrcb)
-#     assert np.array_equal(src2, rgb)
+def test_RGB2YCrCb2RGB_uint8():
+    ctx = cl.create_some_context(0)
+    src1 = np.asarray(Image.open(test_image))
+    ycrcb = RGB2YCrCb(ctx, src1)
+    rgb = YCrCb2RGB(ctx, ycrcb)
+    assert np.array_equal(src1, rgb)
+
+def test_RGB2YCrCb2RGB_float32():
+    ctx = cl.create_some_context(0)
+    src2 = mpimg.imread(test_image)
+    ycrcb = RGB2YCrCb(ctx, src2)
+    rgb = YCrCb2RGB(ctx, ycrcb)
+    assert np.array_equal(src2, rgb)
